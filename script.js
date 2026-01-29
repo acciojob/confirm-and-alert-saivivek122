6c95f7047b405acaf3d8ce7e793e611f6144a995
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-01-29 10:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const result = confirm("Do you want to proceed?");
+
+if (result) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
